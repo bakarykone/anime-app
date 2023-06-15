91c4073e54d034dc861fb4d1c4491b21099f0c32
bakarykone <bakarykone729@gmail.com>
2023-06-15 13:51:20 +0200

    auth middlewares added and implemented to  controllers

---
+++ backend/middleware/auth.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+//le troisième argument next représente le fait de passer à la suite car notre middleware s'éxécute dans une fonction et pour dire qu'après que le middleware a été utilisé on place le next
+const auth = async (req, res, next) => {
+  try {
+    // on va vérifier si le user est réellement la personne qu'il doit être et pour ça on va utiliser le jsonwebtoken après que le user s'est inscrit ou s'est connecté il aura un token spécifique et lorsqu'il voudra faire quelque chose comme liker un post ou supprimer un post on va vérifier si son token est valide
+    const token = req.headers.authorization.split("")[1]; //comme on veut seulement le token qui est placé à l'index 1 du tableau après qu'on les split
+
+    // on va avoir 2 sortes de token un de google auth et un qui nous appartient
+    // on va donc si c'est le notre ou celui de google auth
+    const isCustomAuth = token.length < 500; // si le token est inférieur à 500 caractères il s'agit du notre sinon il s'agit de celui de google auth
+
+    let decodedData; // il s'agit de la donné qu'on veut token lui même
+
+    //Voici comment on obtien le user id si on travaille avec notre propre token
+    if (token && isCustomAuth) {
+      //si on a un token et que c'est le notre alors on  défini notre decodedData
+      decodedData = jwt.verify(token, process.env.SECRET); // ça va nous donner la donné spécifique de chaque token, comme le username d'une personne et son id
+
+      // maintenant qu"on a le decodedData on sait qu'elle est le user qui est connecté et quel user a liké ou supprimer un post
+      // on va stocker son id
+      req.userId = decodedData?.id;
+    } else {
+      decodedData = jwt.decode(token);
+      // le sub est un nom google pour un id spécifique qui déférencie chaque user de google il s'agit d'un id
+      req.userId = decodeData?.sub;
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default auth;
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getPosts, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
+import  auth  from "../middleware/auth.js";
 
 const router = express.Router(); // pour la gestion des routes
 //router.request("path","function")
 router.get("/", getPosts);
-router.post("/", createPost)
-router.patch("/:id", updatePost) //puisque qu'on souhaite connaître l'id avant de modifier le post
-router.delete("/:id", deletePost)
-router.patch("/:id/likePost", likePost)
+router.post("/",auth, createPost)
+router.patch("/:id",auth, updatePost) //puisque qu'on souhaite connaître l'id avant de modifier le post
+router.delete("/:id",auth, deletePost)
+router.patch("/:id/likePost",auth, likePost) // tout le monde peut liker un post mais ne peux le faire qu'une seul foi c'est pour ça qu'on utilise le auth avant le likePost
 export default router;
