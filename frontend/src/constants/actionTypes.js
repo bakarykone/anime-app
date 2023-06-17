be8f3c811b48e53726ec6eae366c89777295a541
bakarykone <bakarykone729@gmail.com>
2023-06-17 16:56:56 +0200

    new actions added

--- frontend/src/constants/actionTypes.js
+++ frontend/src/constants/actionTypes.js
@@ -5,3 +5,7 @@ export const DELETE = "DELETE";
 export const LIKE = "LIKE";
 export const AUTH = "AUTH"
 export const LOGOUT = "LOGOUT"
+export const FETCH_BY_SEARCH = "FETCH_BY_SEARCH";
+export const START_LOADING = "START_LOADING"
+export const END_LOADING = "END_LOADING"
+export const FETCH_POST = "FETCH_POST"
