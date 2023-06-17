10b7d8092c801ab83796d91557645956b7c2be75
bakarykone <bakarykone729@gmail.com>
2023-06-17 16:56:35 +0200

    isLoading added and Post component updated

--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,25 +1,34 @@
 import React from "react";
-import { Grid, CircularProgress} from "@material-ui/core"
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux"; // pour récuperer les données du "state"
 
 import Post from "./Post/Post";
 import useStyles from "./styles";
 
 const Posts = ({ setCurrentId }) => {
-  const posts = useSelector((state) => state.posts); // on sait qu'il se nomme posts car si on va dans "reducers/index.js" on peut aperçevoir notre posts
+  const { posts, isLoading } = useSelector((state) => state.posts); // on sait qu'il se nomme posts car si on va dans "reducers/index.js" on peut aperçevoir notre posts// avant on avait un tableau de post mais maintenant on a un objet posts
   const classes = useStyles();
+
+  if (!posts.length && !isLoading) return "Aucun posts";
 
   console.log(posts);
 
   return (
     // si on a pas de posts on retourne un spinner sinon elle affiche une grid de posts
-    !posts.length ? <CircularProgress/> : (
-      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-        {posts.map((post) => (
-            <Grid key={post.id} item xs={12} sm={6} md={6}>
-              <Post post={post} setCurrentId={setCurrentId}/>
-            </Grid>
-          ))}
+    isLoading ? (
+      <CircularProgress />
+    ) : (
+      <Grid
+        className={classes.container}
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
+        {posts?.map((post) => (
+          <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
+            <Post post={post} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
       </Grid>
     )
   );
